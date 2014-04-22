7a6d56f37f842abd8bf85ad3d006e6c6796364dc
Jeff Ong <jonger4@gmail.com>
2014-04-22 17:37:51 -0400

    added snaptonote core functionality

--- js/main.js
+++ js/main.js
@@ -27,34 +27,46 @@ var mapRange = function(from, to, num) {
 var Music = function(_baseFreq) {
 	var baseFreq = _baseFreq;
 
-	var Major = [2, 2, 1, 2, 2, 2, 1];
+	var majorForwards = [2, 2, 1, 2, 2, 2, 1];
+	var majorBackwards = [-1, -2, -2, -2, -1, -2, -2];
+
+	var index = 1;
+
 	this.scale = [];
 	this.scale[0] = baseFreq;
 
-	for (var i = 1; i <= Major.length; i++){
-		this.scale[i] = this.scale[i-1] * twelthRootOf(Major[i-1]);
+	for (var i = 0; i < majorForwards.length; i++){
+		this.scale[index] = this.scale[index-1] * twelthRootOf(majorForwards[index-1]);
+		index++;
 	}
+	for (var i = 1; i <= majorBackwards.length; i++){
+		if(i == 1) this.scale[i+index] = this.scale[0] * twelthRootOf(majorBackwards[i - 1]);
+		else this.scale[i+index] = this.scale[i+index-1] * twelthRootOf(majorBackwards[i - 1]);
+	}
+
+	this.scale.sort(function(a,b){
+		return a - b;
+	});
+
+	this.scale.pop();
 }
 
-Music.prototype.transform = function(transform) {
-	this.baseFreq
+Music.prototype.snapToNote = function(valueToBeTransformed) {
+	var mappedVal = mapRange([0,256], [this.scale[0], this.scale[this.scale.length-1]], valueToBeTransformed)
+
+	var runningClosest = 10000;
+	var returnNoteIndex;
+
+	for(var i = 0; i < this.scale.length; i++){
+		var difference = Math.abs(this.scale[i] - mappedVal);
+		if (difference < runningClosest) {
+			runningClosest = difference;
+			returnNoteIndex = i; 
+		}
+	}
+
+	return this.scale[returnNoteIndex];
 }
-
-// Music.prototype.getFreq = function (val) {
-// 	mapRange()
-// }
-
-// Music.prototype.changeRoot = function(freq) {
-// 	this.baseFreq = freq;
-// }
-
-// Music.prototype.changeScale = function() {
-	
-// }
-
-// Music.prototype.changeMode = function (argument) {
-// 	// body...
-// }
 
 ////////////////////////////////////////////////////////////////////////////////
 //																			  //
